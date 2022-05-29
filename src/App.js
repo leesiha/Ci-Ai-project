47badc4da9fabca61243fd8627b9b5f8ec56c1af
leesiha <mlice1030@soongsil.ac.kr>
2022-05-29 11:57:00 +0900

    applied bootstrap example design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p> 편집을 시작해봅시다 근데 어딜 건드려야할지 기억이 잘 안 남</p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
     </div>
   );
 }
