aad0c6e8b596935f2355591660e0715a2e414c72
heemin728 <idkhm0728@naver.com>
2022-06-06 14:01:24 +0900

    test commit

--- src/App.js
+++ src/App.js
@@ -4,21 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p> 편집을 시작해봅시다 근데 어딜 건드려야할지 기억이 잘 안 남</p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
     </div>
   );
 }
